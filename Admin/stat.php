2d37ee8a386fcd7a93532686f23fac771e5b496f
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-02-20 10:01:57 +0700

    drop

--- Admin/stat.php
+++
@@ -1,6 +0,0 @@
-
-<?php include "up1.php";
-
-include "../include/koneksi.php";?>
-
-<?php include "down1.php";?>
--- css/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="footer">
-<div id="footerkiri">
-<a href="http://fti.mercubuana-yogya.ac.id/" target="_blank" >FTI Mercu Buana Yogyakarta</a></div>
-<div id="footerkanan">
-
-</div>
-</div>
-</body>
-</html>
